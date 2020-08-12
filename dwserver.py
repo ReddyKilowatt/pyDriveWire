db64475ec597cae7ae8f1701144fbab852a5f30f
Tony Cappellini <cappy2112@gmail.com>
2020-08-11 23:05:27 -0700

    Commented out printing the lsn value, it is a debug line only

--- dwserver.py
+++ dwserver.py
@@ -161,8 +161,8 @@ class DWServer:
             # (disk, lsn) = unpack(">BI", info[0] + NULL + info[1:])
             # Python 3
             disk = info[0]
-            lsn = unpack(">I", NULL + info[1:])[0]
-            print(f'lsn: {lsn}')
+            lsn = unpack(">I", NULL + info[1:])[0] # Python 3
+            # print(f'lsn: {lsn}') # for debugging Python 2 to 3 issues
             pass
         data = NULL_SECTOR
         if self.files[disk] is None:
