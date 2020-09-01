58b96a8650749823234c99b818d6f75ec1d86870
Anthony Cappellini <Anthony.Cappellini@kioxia.com>
2020-08-31 17:32:13 -0700

    Added read_sector() to hide the details of reading a sector from the caller

--- test/test_opreadex.py
+++ test/test_opreadex.py
@@ -18,7 +18,6 @@ COCO_DISK_SECTOR_SIZE = 256
 
 
 def test_disktester(disk_image, cs):
-
     # TODO - let pytest use illegal drive numbers to check for errors
     drive_number = 0
 
@@ -52,6 +51,44 @@ def test_disktester(disk_image, cs):
 
         # print((f'\nCompared {lsn} sectors rc={rc}'))
         # fh.close() # automatically handled using with construct
+
+
+def test_disktester_auto(disk_image, cs):
+    # TODO - let pytest use illegal drive numbers to check for errors
+    drive_number = 0
+
+    with open(disk_image, 'rb') as fh:
+        rc = E_OK
+        lsn = 0
+        while rc == E_OK:
+            rc = read_sector(cs, fh, lsn, drive_number)
+            lsn += 1
+            print((f'Compared {lsn} sectors, rc={rc}'))
+
+
+def read_sector(cs, fh, lsn, drive_number):
+    disk_data = fh.read(COCO_DISK_SECTOR_SIZE)
+    disk_checksum = dwCrc16(disk_data)  # data coming back on first loop matches Python 2  b'\xff\x00'
+    print(f'disk_checksum:{disk_checksum}')
+
+    # Send opcode of next cmd to DW server
+    # DW SPEC: Readex is a 5-byte transaction
+    cs.send(OP_READEX)  # Readex Byte 0
+    # send disk number  # DW SPEC: must be 0-255
+    cs.send(pack(">I", drive_number)[-1:])  # Readex Byte 1
+    # send lsn
+    cs.send(pack(">I", lsn)[-3:])  # Readex Bytes 2,3,4
+
+    server_data = cs.recv(COCO_DISK_SECTOR_SIZE)
+    # print(f'data:{server_data}\nlen of data received: {len(server_data)}')
+    server_checksum = dwCrc16(server_data)
+    # Write the CRC
+    cs.send(server_checksum)
+    # Get the RC
+    rc = cs.recv(1)  # Python 3
+    print(f'rc={rc}, lsn={lsn} disk_crc={hex(unpack(">H", disk_checksum)[0])} server_crc={hex(unpack(">H", server_checksum)[0])}\n')
+    assert (disk_checksum == server_checksum)
+    return rc
 
 
 def server_init(cs):
@@ -99,7 +136,8 @@ def main():
     if cs is not None:
         server_init(cs)
         for ioWrapperObj in parsed_args.file_name:
-            test_disktester(ioWrapperObj.name, cs)
+            # test_disktester(ioWrapperObj.name, cs)
+            test_disktester_auto(ioWrapperObj.name, cs)
     else:
         print('ERROR: Socket initialization was not successfull\n')
 
