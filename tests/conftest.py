41a8c13113298eaa29191a9e925396288765bfe9
Reddy Kilowatt <cappy2112@gmail.com>
2020-10-26 20:31:58 -0700

    Added conftest.py with lines commented out from another repo, to serve as a guide getting this repo configured for pytests. Added tests\__init__.py for pytest

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+
+# from ..src.game_board import GameBoard
+# from ..src.game_player import Player
+# from ..src.tic_tac_toe_lib import TicTacToeGame
+#
+#
+# @pytest.fixture
+# def game_object():
+#     return TicTacToeGame()
+#
+#
+# @pytest.fixture
+# def board(game_object):
+#     return GameBoard(game_object._max_board_locations)
+#
+#
+# def foo():
+#     """
+#     This doesn't do anything, but the Player class needs a function passed in
+#     :return:
+#     """
+#     pass
+#
+#
+# @pytest.fixture
+# def players():
+#     p1 = Player('PLAYER', 'X', foo)
+#     p2 = Player('COMPUTER', 'O', foo)
+#     return p1, p2
