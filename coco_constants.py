c20300976abbf29b037afd13730926eda0d03404
Reddy Kilowatt <cappy2112@gmail.com>
2020-10-28 09:06:57 -0700

    Added constants for 4 of the more common floppy disk formats

--- coco_constants.py
+++ coco_constants.py
@@ -4,4 +4,10 @@ COCO_SECTOR_SIZE = 256
 COCO_DEFAULT_DISK_SIZE = 630
 COCO_HDBDOS_NUMDISKS = 256
 
+# Floppy disk sizes
+SS_35TRK = 630
+SS_40TRK = 720
+DS_40TRK = 1440
+DS_80TRK = 2880
+
 # vim: ts=4 sw=4 sts=4 expandtab
