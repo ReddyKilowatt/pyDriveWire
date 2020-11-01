b0da5cbbb009624a0e8cd4227dcdb32a1307fe0f
Reddy Kilowatt <cappy2112@gmail.com>
2020-10-31 20:18:16 -0700

    For easy running the server and its options

---
+++ runserverpy3.bat
@@ -0,0 +1,5 @@
+@echo off
+REM run pydrivewire in Python 3 (port 65503) using the config file for the Python3 version of pyDriveWire.py
+REM run with a remote REPL
+REM python pyDriveWire.py -d -a -p 65503 --cmd-port 6809
+python pyDriveWire.py --config c:\Users\z48176zz\.pydrivewirerc_py3
