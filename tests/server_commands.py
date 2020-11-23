22e8705a27b62c0e0315aea2ac0df06ecd00a345
Reddy Kilowatt <cappy2112@gmail.com>
2020-11-22 20:46:44 -0800

    Moved the dw disk commands into their own class, making ServerCommand leaner. Added ServerConfigCommands for ServerCommand to inherit from

--- tests/server_commands.py
+++ tests/server_commands.py
@@ -1,14 +1,65 @@
 from remote_repl import RemoteRepl
 
 
-class ServerCommand:
-    def __init__(self, debug_opt):
-        self._repl = RemoteRepl(debug=debug_opt)
-        self._write = self.repl.write
-        self._read = self.repl.read
+class ServerConfigCommands:
 
-    def disk_show(self):
-        self._write('dw disk show')
-        read_data = self._read()
-        print(f'{read_data}')
-        # return read_data
+    def _config_common(self, repl_command, print_output=True):
+        self._write(f'dw config {repl_command}')
+        data = self._read()
+        if print_output:
+            print(f'{data}')
+        return data  # the caller may want to parse the output for specific content
+
+    def config_show(self, print_output=True):
+        return self._config_common('show', print_output)
+
+
+class ServerDiskCommands:
+
+    def _disk_common(self, repl_command, print_output=True):
+        self._write(f'dw disk {repl_command}')
+        data = self._read()
+        if print_output:
+            print(f'{data}')
+        return data  # the caller may want to parse the output for specific content
+
+    def disk_show(self, print_output=True):
+        return self._disk_common('show', print_output)
+
+    def disk_create(self, drive_number, disk_filename):
+        return self._disk_common(f'disk {drive_number} {disk_filename}')
+
+    def disk_eject(self, drive_number):
+        return self._disk_common(f'disk eject {drive_number}')
+
+    def disk_insert(self, drive_number, path):
+        return self._disk_common(f'disk insert {drive_number} {path}')
+
+    def disk_info(self, drive_number):
+        return self._disk_common(f'disk info {drive_number}')
+
+
+class ServerCommand(ServerDiskCommands, ServerConfigCommands):
+    """
+    The ServerCommand class provides a simple way for the caller
+    to send commands to the pyDriveWire server
+    """
+
+    def __init__(self, debug_opt=False):
+        self._repl = RemoteRepl(debug=debug_opt)
+        self._write = self._repl.write
+        self._read = self._repl.read
+
+    def _cmd_common(self, repl_command, print_output=True):
+        self._write(f'dw {repl_command}')
+        data = self._read()
+        if print_output:
+            print(f'{data}')
+        return data  # the caller may want to parse the output for specific content
+
+    # def disk_show(self, print_output=True):
+    #     return self._disk_cmd_common('show', print_output)
+
+
+sc = ServerCommand()
+sc.config_show()
