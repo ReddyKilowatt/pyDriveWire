baa69d2c33868bb047cf9cd9a6f15858bd6bf9ed
Reddy Kilowatt <cappy2112@gmail.com>
2020-11-22 17:30:50 -0800

    Added the remote REPL port to runserverpy3/bat

--- runserverpy3.bat
+++ runserverpy3.bat
@@ -3,4 +3,8 @@ REM run pydrivewire in Python 3 (port 65503) using the config file for the Pytho
 REM run with a remote REPL
 REM python pyDriveWire.py -d -a -p 65503 --cmd-port 6809
 REM python pyDriveWire.py --config c:\Users\z48176zz\.pydrivewirerc_py3  3dbrick.dsk
-python pyDriveWire.py --config c:\Users\z48176zz\.pydrivewirerc_py3 %1
+REM python pyDriveWire.py --config c:\Users\z48176zz\.pydrivewirerc_py3 %1
+
+REM enable the remote REPL port, 6809
+python pyDriveWire.py -D 6809 --config c:\Users\z48176zz\.pydrivewirerc_py3 %1
+
